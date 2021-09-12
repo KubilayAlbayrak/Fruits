aecb1f3caa1f68351a639b5939d8a624fffc6856
Kubilay <skubilayalbayrak@gmail.com>
2021-09-12 17:02:16 +0300

    Initial Commit

---
+++ Shared/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Shared
+//
+//  Created by Kubilay Albayrak on 12.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
